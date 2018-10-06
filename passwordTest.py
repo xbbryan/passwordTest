71c60ec84b4da4e71be6dbc6cd4f439c204c8d1a
Bryan <xbbryan@yahoo.com.tw>
2018-10-06 16:58:10 +0800

    密碼測試程式 3次鎖定

---
+++ passwordTest.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+chance = 3
+
+while chance <= 3 and chance > 0:   
+    userpassword = input('請輸入密碼: ')
+    if userpassword == password:
+        print('登入成功！')
+    else:
+        chance = chance - 1
+        if chance < 1:
+            break
+        print('密碼錯誤！還有', chance, '次機會')
+print('輸入錯誤次數超過3次，此帳號已暫時鎖定')
+        
